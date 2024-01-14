9b225d441eab4fb6a7c25f707589042765a32f35
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-01-14 17:59:14 +0300

    add right bar with search input

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,27 @@
 import ActionsBar from "./ActionsBar";
 import Feed from "./Feed";
+import RightBar from "./RightBar";
 import "./styles/App.css";
 
 function App() {
   return (
-    <>
+    <div className="mt-2">
       <div className="flex justify-center h-screen w-screen">
         <div className="flex-grow flex max-w-6xl h-full">
           <div className="flex items-center justify-between flex-grow h-full card rounded-box">
             <ActionsBar />
           </div>
           <div className="divider"></div>
-          <div className="flex items-center justify-between h-full grow-[2] card rounded-box">
+          <div className="flex items-center justify-between h-full grow-[3] card rounded-box">
             <Feed />
           </div>
           <div className="divider"></div>
           <div className="flex items-center justify-between flex-grow h-full card rounded-box">
-            content
+            <RightBar />
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
---
+++ client/src/RightBar.tsx
@@ -0,0 +1,13 @@
+const RightBar = () => {
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search"
+        className="input input-ghost w-full max-w-xs bg-gray-800 rounded-full"
+      />
+    </div>
+  );
+};
+
+export default RightBar;
