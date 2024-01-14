5b1ff63403fe2fea416c439919609503d753fd04
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-01-15 00:04:39 +0300

    create the explore feed with all posts

--- client/src/components/ExploreFeed.tsx
+++ client/src/components/ExploreFeed.tsx
@@ -1,4 +1,8 @@
 import usePosts from "../services/usePosts";
+import defaultAvatar from "../assets/default-twitter-avatar.png";
+import { getTwitterTime } from "twitter-time";
+import { MdMoreHoriz } from "react-icons/md";
+import { LuDot } from "react-icons/lu";
 
 const ExploreFeed = () => {
   const { data: posts, error, isLoading } = usePosts();
@@ -9,7 +13,28 @@ const ExploreFeed = () => {
   return (
     <div>
       {posts?.map((post) => (
-        <div key={post.id}>{post.content}</div>
+        <div className="prose" key={post.id}>
+          <div className="flex gap-2 items-center justify-between max-h-14">
+            <div className="flex gap-2 items-center">
+              <img
+                className="rounded-full w-12"
+                src={defaultAvatar}
+                alt="defaultAvatar"
+              />
+              <div className="font-bold text-white p-1 hover-effect">
+                {post.User.username}
+              </div>
+              <div className="flex items-center text-sm text-gray-500">
+                @{post.User.username} <LuDot /> {getTwitterTime(post.createdAt)}
+              </div>
+            </div>
+            <div className="hover-effect p-1 text-2xl">
+              <MdMoreHoriz />
+            </div>
+          </div>
+          <p>{post.content}</p>
+          <div className="divider"></div>
+        </div>
       ))}
     </div>
   );
