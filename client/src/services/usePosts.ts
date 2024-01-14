d55e7735d83a69eee373f5af2886ca4db0144375
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-01-14 23:07:42 +0300

    create custom hook to fetch posts

---
+++ client/src/services/usePosts.ts
@@ -0,0 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const usePosts = (userId: string | undefined) => {
+  const fetchPosts = () => axios.get<Post[]>("/").then((res) => res.data);
+
+  return useQuery<Post[], Error>({
+    queryKey: userId ? ["posts", userId] : ["posts"],
+    queryFn: fetchPosts,
+    staleTime: 10 * 1000,
+  });
+};
+
+interface Post {
+  id: string;
+  content: string;
+  userId: string;
+}
+
+export default usePosts;
