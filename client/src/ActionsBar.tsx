89abb7bb5906b6c28cf18122c1245d0c81266499
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-01-14 17:20:04 +0300

    add actions bar icons

---
+++ client/src/ActionsBar.tsx
@@ -0,0 +1,42 @@
+import { CgMoreO } from "react-icons/cg";
+import { CiBookmark } from "react-icons/ci";
+import { GiThunderSkull } from "react-icons/gi";
+import { GoHomeFill } from "react-icons/go";
+import { GrNotification } from "react-icons/gr";
+import { HiOutlineEnvelope } from "react-icons/hi2";
+import { IoPersonOutline, IoSearchOutline } from "react-icons/io5";
+import { MdOutlineWorkspacePremium } from "react-icons/md";
+import { RiGroupLine } from "react-icons/ri";
+
+const ActionsBar = () => {
+  const actions: { label: string; value: JSX.Element }[] = [
+    { label: "", value: <GiThunderSkull size="2.5rem" /> },
+    { label: "Home", value: <GoHomeFill /> },
+    { label: "Explore", value: <IoSearchOutline /> },
+    { label: "Notifications", value: <GrNotification /> },
+    { label: "Messages", value: <HiOutlineEnvelope /> },
+    { label: "Bookmarks", value: <CiBookmark /> },
+    { label: "Community", value: <RiGroupLine /> },
+    { label: "Premium", value: <MdOutlineWorkspacePremium /> },
+    { label: "Profile", value: <IoPersonOutline /> },
+    { label: "More", value: <CgMoreO /> },
+  ];
+
+  return (
+    <div className="flex flex-col h-full lg:w-56">
+      {actions.map((action, index) => (
+        <div
+          className="flex items-center gap-2 p-3 m-1 rounded-3xl text-2xl hover:font-bold hover:cursor-pointer hover:bg-gray-800 transition duration-300 ease-in-out"
+          key={index}
+        >
+          {action.value} {action.label}
+        </div>
+      ))}
+      <button className="btn font-bold bg-sky-500 rounded-full mt-5 btn-xs sm:btn-sm md:btn-md lg:btn-md lg:text-xl hover:bg-sky-800">
+        Post
+      </button>
+    </div>
+  );
+};
+
+export default ActionsBar;
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,24 @@
+import ActionsBar from "./ActionsBar";
 import "./styles/App.css";
 
 function App() {
   return (
     <>
-      <div className="text-lg p-5">Hello</div>
-      <button className="btn btn-lg">Button</button>
+      <div className="flex justify-center h-screen w-screen">
+        <div className="flex-grow flex max-w-6xl h-full">
+          <div className="flex items-center justify-between h-full card rounded-box">
+            <ActionsBar />
+          </div>
+          <div className="divider"></div>
+          <div className="grid h-full flex-grow card bg-base-300 rounded-box place-items-center">
+            content
+          </div>
+          <div className="divider"></div>
+          <div className="grid h-full flex-grow card bg-base-300 rounded-box place-items-center">
+            content
+          </div>
+        </div>
+      </div>
     </>
   );
 }
