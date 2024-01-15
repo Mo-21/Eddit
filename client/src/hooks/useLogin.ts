00713435c30f45878a8460ef8f3e14ec8223990a
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-01-16 01:44:46 +0300

    redirect user ot homepage on success

--- client/src/hooks/useLogin.ts
+++ client/src/hooks/useLogin.ts
@@ -1,14 +1,18 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { Credentials, UserResponse } from "../services/authService";
 import authService from "../services/authService";
+import { useNavigate } from "react-router-dom";
 
 const useLogin = () => {
   const queryClient = useQueryClient();
+
+  const navigate = useNavigate();
 
   return useMutation<UserResponse, Error, Credentials>({
     mutationFn: authService.logIn,
     onSuccess: (userResponse, credentials) => {
       queryClient.setQueryData<UserResponse>(["user"], userResponse);
+      navigate("/");
     },
   });
 };
