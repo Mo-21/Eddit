aa1105930cbd56bd0ce3431ba42624ef1cf4843e
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-01-16 01:26:35 +0300

    add custom method for loging in

--- client/src/hooks/useLogin.ts
+++ client/src/hooks/useLogin.ts
@@ -1,29 +1,16 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import axios from "axios";
+import { Credentials, UserResponse } from "../services/authService";
+import authService from "../services/authService";
 
 const useLogin = () => {
   const queryClient = useQueryClient();
 
   return useMutation<UserResponse, Error, Credentials>({
-    mutationFn: (data: Credentials) =>
-      axios.post<UserResponse>("/api/auth/login", data).then((res) => res.data),
+    mutationFn: authService.logIn,
     onSuccess: (userResponse, credentials) => {
       queryClient.setQueryData<UserResponse>(["user"], userResponse);
     },
   });
 };
-
-interface UserResponse {
-  id: number;
-  username: string;
-  email: string;
-  avatar: string | null;
-  createdAt: string;
-}
-
-interface Credentials {
-  email: string;
-  password: string;
-}
 
 export default useLogin;
