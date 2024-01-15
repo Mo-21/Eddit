3c95b09472e42697409e0f99f116785ec2cc33b2
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-01-15 03:02:11 +0300

    update login schema

--- client/src/validationSchema.ts
+++ client/src/validationSchema.ts
@@ -7,22 +7,7 @@ export const loginSchema = z.object({
     .max(255, "Email can be max 255 char")
     .min(10, "Email can be min 255 char"),
 
-  password: z
-    .string()
-    .min(1, "Password is Required")
-    .min(8, "Password must be at least 8 characters long")
-    .refine((value) => /\d/.test(value), {
-      message: "Password must contain at least one number",
-    })
-    .refine((value) => /[a-z]/.test(value), {
-      message: "Password must contain at least one lowercase letter",
-    })
-    .refine((value) => /[A-Z]/.test(value), {
-      message: "Password must contain at least one uppercase letter",
-    })
-    .refine((value) => /[^a-zA-Z\d]/.test(value), {
-      message: "Password must contain at least one special character",
-    }),
+  password: z.string(),
 });
 
 export type LoginSchemaType = z.infer<typeof loginSchema>;
